bb8f52d5f38d48bdc43435f9dff93340e8e4245b
Oukangneng <oaoconnell@gmail.com>
2025-02-11 20:43:24 -0500

    Update config.js

--- config.js
+++ config.js
@@ -107,7 +107,7 @@ const config = {
         {
             id: 'stage5-swipe2',
             title: 'Final Slide',
-            description: '<span style="font-size:24px">INSERT FACT.</span><br><br><span style="font-size:18px"><i>How Can You Get Involved?</i></span><br><br>The following organizations fight for supporting political and religious prisoners in China:<br></p><ul><li><a target = "_blank" href="https://www.cecc.gov/resources/political-prisoner-database/">Congressional-Executive Commsion on China’s Political Prisoner Database</a></li><li><a target = "_blank" href="https://duihua.org/">Dui Hua Foundationr</a></li><li><a target = "_blank" href="https://www.nchrd.org/">Chinese Human Rights Defendersl</a></li></ul>',
+            description: '<span style="font-size:24px">INSERT FACT.</span><br><br><span style="font-size:18px"><i>How Can You Get Involved?</i></span><br><br>The following organizations fight for supporting political and religious prisoners in China:<br></p><ul><li><a target = "_blank" href="https://www.cecc.gov/resources/political-prisoner-database/">Congressional-Executive Commsion on China’s Political Prisoner Database</a></li><li><a target = "_blank" href="https://duihua.org/">Dui Hua Foundationr</a></li><li><a target = "_blank" href="https://www.nchrd.org/">Chinese Human Rights Defenders</a></li></ul>',
             location: {
                 center: [116.4074, 39.9042], // Beijing
                 zoom: 2,
